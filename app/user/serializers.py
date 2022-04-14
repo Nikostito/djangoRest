f94c0ef18b9f0bf1c2296e4eec88fbc02ba0750b
nikostito <nikostito@me.com>
2022-04-14 16:06:24 +0300

    add endpoints for tags & add tag serializers

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,28 +16,37 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """ Update user info and password"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
-    """ Serializer for the user auth object """
+    """Serializer for the user authentication object"""
     email = serializers.CharField()
     password = serializers.CharField(
-        style={
-            'input_type': 'password'},
+        style={'input_type': 'password'},
         trim_whitespace=False
     )
 
     def validate(self, attrs):
-        """ Validate the user and auth """
+        """Validate and authenticate the user"""
         email = attrs.get('email')
         password = attrs.get('password')
+
         user = authenticate(
             request=self.context.get('request'),
             username=email,
             password=password
-
         )
         if not user:
-            msg = _('Unable to authenticate with invalid credentials')
-            raise serializers.ValidationError(msg, code='authentication')
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+
         attrs['user'] = user
         return attrs
